8d5db8ab1f8f2c5cd81ead387b0aeba60d28c922
kakembohenri <kakembohenry5@gmail.com>
2025-07-13 00:02:41 +0300

    Add email sending

---
+++ src/app/api/send-email/route.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from "next/server";
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: parseInt(process.env.SMTP_PORT || "587"),
+  secure: process.env.SMTP_PORT === "465", // Use SSL if port is 465
+  auth: {
+    user: process.env.SMTP_USERNAME,
+    pass: process.env.SMTP_PASSWORD,
+  },
+});
+
+export async function POST(req: NextRequest) {
+  const body: any = await req.json();
+  const { to, subject, htmlContent } = body;
+
+  try {
+    await transporter.sendMail({
+      from: process.env.EMAIL_FROM,
+      to,
+      subject: subject,
+      html: htmlContent,
+    });
+    return NextResponse.json(
+      { message: "Email sent successfully" },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json({ error: "Error sending email" }, { status: 500 });
+  }
+}
